35e2c0701602d0e11e3c105e2ba39456599f8905
Ewa Rostecka-Rzońca <151127505+EwaRRPoland@users.noreply.github.com>
2025-01-10 14:28:36 +0100

    added POM pattern

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,8 +29,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    video: 'retain-on-failure',
+    trace: 'on',
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -40,15 +40,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
